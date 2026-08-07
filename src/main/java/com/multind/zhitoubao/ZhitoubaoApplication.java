62d4be05c5c335c74b657e7a6034d4fcb0dc4bd2
Corbett Zhang <corbettzhang@outlook.com>
2026-08-07 12:03:32 +0800

    build: initialize Java 26 Spring Boot service

---
+++ src/main/java/com/multind/zhitoubao/ZhitoubaoApplication.java
@@ -0,0 +1,12 @@
+package com.multind.zhitoubao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhitoubaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhitoubaoApplication.class, args);
+    }
+}
